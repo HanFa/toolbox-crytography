eeda1cc33dd026b79c6156e0e94b7ca2d939ce04
Fang Han <hanfa@umich.edu>
2018-06-08 16:44:03 +0800

    modify project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='toolbox-crytography',
+setup(name='toolbox-cryptography',
       version='0.1',
       author='Fang Han',
       author_email='hanfa@umich.edu',
